95d0569841ef86c7515349d5d9c2e2e3b7a712c5
sungjinwoo47573-collab <sungjinwoo47573@gmail.com>
2026-02-21 22:07:29 +0530

    Update banner_manager.py

--- utils/banner_manager.py
+++ utils/banner_manager.py
@@ -1,8 +1,8 @@
 class BannerManager:
     # Replace these with your Canva-designed banners
     MAIN_BANNER = "https://image2url.com/r2/default/images/1771684587824-05c296aa-d67d-4cdc-be90-43b8d6a297b6.png"
-    COMBAT_BANNER = "https://i.imgur.com/your_combat_banner.png"
-    ADMIN_BANNER = "https://i.imgur.com/your_admin_banner.png"
+    COMBAT_BANNER = "https://image2url.com/r2/default/images/1771684587824-05c296aa-d67d-4cdc-be90-43b8d6a297b6.png"
+    ADMIN_BANNER = "https://image2url.com/r2/default/images/1771684587824-05c296aa-d67d-4cdc-be90-43b8d6a297b6.png"
 
     @staticmethod
     def apply(embed, type="main"):
